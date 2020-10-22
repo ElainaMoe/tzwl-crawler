9d9d2d8f9593b6e292967cfda9edd5a312712647
Pesy Wu <bili33@87ouo.top>
2020-10-22 18:47:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import datetime
 import csv
 import os
 import time
+from decimal import Decimal
 
 success=0
-start_timestamp=datetime.datetime.now()
+start_timestamp=Decimal(datetime.datetime.now())
 def create_csv(path):
     with open(path,"w+",newline="",encoding="utf8") as file:    # 打开文件，也相当于一个回车，避免覆盖文档
         csv_file = csv.writer(file)
@@ -33,7 +34,7 @@ for i in range(1,99999999999):
         print('获取到内容，正在存储……')
         success=1
         append_csv(filename)
-    end_timestamp=datetime.datetime.now()
+    end_timestamp=Decimal(datetime.datetime.now())
     if int(end_timestamp)-int(start_timestamp)>=19800:
         break
 if success == 0:
