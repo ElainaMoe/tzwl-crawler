b5d39ee679ca6822b3a98660d7da5117d7de2fb0
GamerNoTitle <bili33@87ouo.top>
2020-10-22 13:33:50 +0800

    init

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+import datetime
+import csv
+
+def create_csv(path):
+    with open(path,"w+",newline="",encoding="utf8") as file:    # 打开文件，也相当于一个回车，避免覆盖文档
+        csv_file = csv.writer(file)
+        head = 'data' # 创建csv表头
+        csv_file.writerow(head)
+def append_csv(path):
+    with open(path,"a+",newline='',encoding="utf8") as file:
+        csv_file = csv.writer(file)
+        data = [info]
+        csv_file.writerows(data)
+
+baseurl="http://tzwl.xyz/index/kminfo.html?ddid="
+time = datetime.datetime.now()
+date=str(time.year)+str(time.month)+str(time.day)
+filename="./data/"+date+'.csv'
+create_csv(filename)
+for i in range(1,99999999999):
+    url=baseurl+date+str(i).zfill(11)
+    print(url)
+    result=requests.get(url)
+    info=result.text[1:]
+    if str(info)=='[]':
+        print('获取到空内容，舍弃……')
+    else:
+        print('获取到内容，正在存储……')
+        append_csv(filename)
