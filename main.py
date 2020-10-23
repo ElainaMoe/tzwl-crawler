994178ea7688b2cf3ce2915ec4a5c464a394d872
GamerNoTitle <bili33@87ouo.top>
2020-10-23 11:13:23 +0800

    修复一处变量命名冲突

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import os
 import time
 
 success=0
-start_timestamp=datetime.datetime.now()
-print(type(start_timestamp))
+start_timestamp=time.time()
 def create_csv(path):
     with open(path,"w+",newline="",encoding="utf8") as file:    # 打开文件，也相当于一个回车，避免覆盖文档
         csv_file = csv.writer(file)
-        head = 'data' # 创建csv表头
+        head = ['data'] # 创建csv表头
         csv_file.writerow(head)
 def append_csv(path):
     with open(path,"a+",newline='',encoding="utf8") as file:
@@ -19,8 +18,8 @@ def append_csv(path):
         csv_file.writerows(data)
 
 baseurl="http://tzwl.xyz/index/kminfo.html?ddid="
-time = datetime.datetime.now()
-date=str(time.year)+str(time.month)+str(time.day)
+current_time = datetime.datetime.now()
+date=str(current_time.year)+str(current_time.month)+str(current_time.day)
 filename="./data/"+date+'.csv'
 create_csv(filename)
 for i in range(1,99999999999):
@@ -34,8 +33,7 @@ for i in range(1,99999999999):
         print('获取到内容，正在存储……')
         success=1
         append_csv(filename)
-    end_timestamp=datetime.datetime.now()
-    print(type(end_timestamp))
+    end_timestamp=time.time()
     if int(end_timestamp)-int(start_timestamp)>=19800:
         break
 if success == 0:
